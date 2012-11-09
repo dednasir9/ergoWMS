abc12c4f3f31044cf28d678c222880c256809e20
dednasir9 <nasir.ali9@hotmail.com>
2012-11-09 02:05:54 +0100

    layout changed a bit.

--- ergoWMS/UI/ergoWMS.Master.designer.cs
+++ ergoWMS/UI/ergoWMS.Master.designer.cs
@@ -23,6 +23,24 @@ namespace ErgoWMS.UI {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// TreeMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TreeView TreeMenu;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
